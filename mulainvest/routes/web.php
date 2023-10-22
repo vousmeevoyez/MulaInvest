88d5e143b9dceeb00bd89ffe52b44b3ff50e12d6
RifqiNotWibu <rifqifadhillahh@gmail.com>
2023-10-22 22:19:43 +0700

    Added Register and Login Routes

--- mulainvest/routes/web.php
+++ mulainvest/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Page Registrasi
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register-form');
+// Controller Registrasi
+Route::post('/register', [RegisterController::class, 'register'])->name('register-store');
+
+// Page login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login-form');
+// Controller Login
+Route::post('/login', [LoginController::class, 'login'])->name('login-store');
+// Controller Logout
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Page Home (User)
+
+// Page CMS
+Route::get('/content-management-system', [AdminController::class, 'showCMS'])->name('content-management-system');
